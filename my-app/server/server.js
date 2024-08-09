73abfc2882a5f0ee157b29b1e155b7237f97227d
Chinni Yadav <128020656+Vamsichinni9999@users.noreply.github.com>
2024-08-09 20:05:16 +0530

    first commit

---
+++ my-app/server/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import videoRoutes from "./routes/video.js"
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use("/api/videos", videoRoutes);
+app.listen(port, () => {
+    connectDB();
+    console.log("server started listening on port ",port);
+    
+})
---
+++ my-app/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2mtFBm4u5ptvlYPb5jchvJc7xj1oLvis",
+  authDomain: "upload-files-ded18.firebaseapp.com",
+  projectId: "upload-files-ded18",
+  storageBucket: "upload-files-ded18.appspot.com",
+  messagingSenderId: "786782832365",
+  appId: "1:786782832365:web:703741d3669e35942e6ee4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
